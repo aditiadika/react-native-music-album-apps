fcfa7a31cae05322842597f027b143055b330661
aditiadika <aditiaaadicka@gmail.com>
2018-07-31 18:16:29 +0700

    done in styling page

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/Header'
+
+const App = () => (
+  <View>
+    <Header headerText={'Music Albums'} />
+  </View>
+)
+
+export default App;
