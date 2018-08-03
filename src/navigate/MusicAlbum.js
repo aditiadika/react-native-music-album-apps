3a475ed1a4f8c9a4599a5a834b0fbb2511bb1677
aditiadika <aditiaaadicka@gmail.com>
2018-08-03 15:36:21 +0700

    fix header in album list

--- src/navigate/MusicAlbum.js
+++ src/navigate/MusicAlbum.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import AlbumList from '../components/AlbumList';
 import Header from '../components/Header';
 
-const MusicAlbum = () => (
-    <View style={{ flex: 1 }}>
-      <Header headerText={'Music Albums'} />
-      <AlbumList />
-    </View>
-);
+class MusicAlbum extends Component {
+  static navigationOptions = {
+    header: null
+  }
+  
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Music Albums'} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
 
 export default MusicAlbum;
