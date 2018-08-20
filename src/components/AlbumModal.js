f912825d34983a6ebd989d9484d7bf5f6b4438cb
aditiadika <aditiaaadicka@gmail.com>
2018-08-20 14:29:14 +0700

    setting backend for midtrans

--- src/components/AlbumModal.js
+++ src/components/AlbumModal.js
@@ -1,136 +1,3 @@
-// import React from 'react';
-// import { Modal, Text, Image, View, StyleSheet } from 'react-native';
-// import CardSection from './CardSection';
-// import Card from './Card';
-// import Button from './Button';
-// import Header from './Header';
-
-// const AlbumModal = props => {
-//     const { cb: _this } = props;
-//     const { selectedAlbum } = _this.state;
-
-//     checkOutHandler() {
-        
-//         var optionConect = {
-//             clientKey: "VT-CLIENT-AAAAAAAAAAAAA",
-//             urlMerchant : "http://example.com/payment"
-//         }
-
-//         var transRequest = {
-//             transactionId : "001",
-//             totalAmount : 6000
-//         }
-
-//         var itemDetails = [
-//             {id:"001", price:1000, qty:4, name:"cimory"},
-//             {id:"002", price:2000, qty:2, name:"cofimix"}
-//         ];
-
-//         var creditCardOptions = {
-//             saveCard:false,
-//             saveToken:false,
-//             paymentMode:"Normal",
-//             secure:false
-//         };
-
-//         var userDetail = {
-//             fullName:"Ahmad", email:"ah@mad.com", 
-//             phoneNumber:"0850000000", 
-//             userId:"U01", 
-//             address:"kudus", 
-//             city:"kudus", 
-//             country:"IDN", 
-//             zipCode:"59382"
-//         };
-
-//         var optionColorTheme = {
-//             primary:'#c51f1f',
-//             primaryDark:'#1a4794',
-//             secondary:'#1fce38'
-//         }
-
-//         var optionFont = {
-//             defaultText:"open_sans_regular.ttf", semiBoldText:"open_sans_semibold.ttf",
-//             boldText:"open_sans_bold.ttf"
-//         }
-
-//         var callback = (res)=>{console.log(res)};
-
-//         PaymentMidtrans.checkOut(
-//             optionConect,
-//             transRequest,
-//             itemDetails,
-//             creditCardOptions,
-//             userDetail,
-//             optionColorTheme,
-//             optionFont,
-//             callback(res)
-//         );
-//     };
-
-//     return (
-//         <Modal
-//             animationType="slide"
-//             transparent={false}
-//             visible={props.modalVisible}
-//             onRequestClose={() => { console.log('test'); }}
-//         >
-//             <Card>
-//                 <Header headerText={'Checkout Page'} />
-
-//                 <CardSection>
-//                     <View style={styles.imageContainerStyle}>
-//                         <Image source={{ uri: selectedAlbum.thumbnail_image }} style={styles.thumbnailStyle} />
-//                     </View>
-//                     <View style={styles.headerContentStyle}>
-//                         <Text style={styles.titleTextStyle}>{selectedAlbum.title}</Text>
-//                         <Text>{selectedAlbum.artist}</Text>
-//                     </View>
-//                 </CardSection>
-
-//                 <CardSection>
-//                     <View>
-//                         <Image source={{ uri: selectedAlbum.image }} style={styles.albumImage} />
-//                     </View>
-//                 </CardSection>
-
-//                 <CardSection>
-//                     <Button onPress={() => this.checkOutHandler()}>Checkout</Button>
-//                     <Button onPress={props.onModalClosed}>Cancel</Button>
-//                 </CardSection>
-//             </Card>
-//         </Modal>
-//     );
-// };
-
-// const styles = StyleSheet.create({
-//     headerContentStyle: {
-//         flexDirection: 'column',
-//         justifyContent: 'space-around'
-//     },
-//     titleTextStyle: {
-//         fontSize: 18
-//     },
-//     thumbnailStyle: {
-//         height: 50,
-//         width: 50
-//     },
-//     imageContainerStyle: {
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         marginLeft: 10,
-//         marginRight: 10
-//     },
-//     albumImage: {
-//         height: 300,
-//         width: 320,
-//         marginLeft: 10,
-//         marginRight: 10
-//     }
-// });
-
-// export default AlbumModal;
-
 import React, { Component } from 'react';
 import { Modal, Text, Image, View, StyleSheet } from 'react-native';
 import PaymentMidtrans from 'react-native-midtrans';
@@ -144,17 +11,29 @@ class AlbumModal extends Component {
     checkOutHandler() {
         const optionConect = {
             clientKey: 'SB-Mid-client-MG6GpVypt8HaxKDD',
-            urlMerchant: 'http://www.musikalbum.com:3000/charge/'
+            urlMerchant: 'http://10.0.3.2/'
         };
 
+        // const transRequest = {
+        //     transactionId: '001',
+        //     totalAmount: 6000
+        // };
         const transRequest = {
             transactionId: '001',
             totalAmount: 6000
         };
 
+        // const itemDetails = [
+        //     { id: '001', price: 1000, qty: 4, name: 'cimory' },
+        //     { id: '002', price: 2000, qty: 2, name: 'cofimix' }
+        // ];
         const itemDetails = [
-            { id: '001', price: 1000, qty: 4, name: 'cimory' },
-            { id: '002', price: 2000, qty: 2, name: 'cofimix' }
+            {
+                id: 'A01',
+                price: 280000,
+                qty: 1,
+                name: 'Mie Ayam Komplit'
+            }
         ];
 
         const creditCardOptions = {
@@ -164,14 +43,26 @@ class AlbumModal extends Component {
             secure: false
         };
 
-        const userDetail = {
-            fullName: 'Ahmad', 
-            email: 'ah@mad.com', 
-            phoneNumber: '0850000000', 
-            userId: 'U01', 
-            address: 'kudus', 
-            city: 'kudus', 
-            country: 'IDN', 
+        // const userDetail = {
+        //     fullName: 'Ahmad', 
+        //     email: 'ah@mad.com', 
+        //     phoneNumber: '0850000000', 
+        //     userId: 'U01', 
+        //     address: 'kudus', 
+        //     city: 'kudus', 
+        //     country: 'IDN', 
+        //     zipCode: '59382'
+        // };
+        const customer_details = {
+            fullName: 'Ahmad',
+            email: 'tester@example.com', 
+            first_name: 'Budi', 
+            last_name: 'Khannedy', 
+            phoneNumber: '628112341234',
+            userId: 'U01',
+            address: 'kudus',
+            city: 'kudus',
+            country: 'IDN',
             zipCode: '59382'
         };
 
@@ -189,19 +80,33 @@ class AlbumModal extends Component {
 
         const callback = res => { console.log('landing here', res); };
 
+        const tes = {
+            optionConect,
+            transRequest,
+            itemDetails,
+            creditCardOptions,
+            customer_details,
+            optionColorTheme,
+            optionFont,
+        };
+
+        console.log('check here', tes);
+
         PaymentMidtrans.checkOut(
             optionConect,
             transRequest,
             itemDetails,
             creditCardOptions,
-            userDetail,
+            customer_details,
             optionColorTheme,
             optionFont,
-            callback()
+            callback(res)
         );
     }
 
     render() {
+        const { cb: _this } = this.props;
+        const { selectedAlbum } = _this.state;        
         return (
             <Modal
                 animationType="slide"
@@ -215,19 +120,19 @@ class AlbumModal extends Component {
                     <CardSection>
                         <View style={styles.imageContainerStyle}>
                             <Image 
-                                source={{ uri: this.thumbnail_image }} 
+                                source={{ uri: selectedAlbum.thumbnail_image }} 
                                 style={styles.thumbnailStyle} 
                             />
                         </View>
                         <View style={styles.headerContentStyle}>
-                            <Text style={styles.titleTextStyle}>{this.title}</Text>
-                            <Text>{this.artist}</Text>
+                            <Text style={styles.titleTextStyle}>{selectedAlbum.title}</Text>
+                            <Text>{selectedAlbum.artist}</Text>
                         </View>
                     </CardSection>
     
                     <CardSection>
                         <View>
-                            <Image source={{ uri: this.image }} style={styles.albumImage} />
+                            <Image source={{ uri: selectedAlbum.image }} style={styles.albumImage} />
                         </View>
                     </CardSection>
     
